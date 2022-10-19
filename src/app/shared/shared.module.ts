37fca3603e756c87596f8f229fd5d60d1da8e16a
Krutilla <Eszter.Krutilla@t-systems.com>
2022-10-19 15:48:15 +0200

    add folder structure, components, ngPrime import

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ngPrimeModule } from '../ngPrime/ngPrime.module';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
+import { CustomCheckboxComponent } from './components/custom-checkbox/custom-checkbox.component';
+
+@NgModule({
+  declarations: [
+    CustomButtonComponent,
+    // CustomInputComponent,
+    // CustomCheckboxComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, ngPrimeModule],
+  exports: [
+    ReactiveFormsModule,
+    ngPrimeModule,
+    CustomButtonComponent,
+    // CustomInputComponent,
+    // CustomCheckboxComponent,
+  ],
+})
+export class SharedModule {}
